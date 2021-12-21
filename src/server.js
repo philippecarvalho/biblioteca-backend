54e5df5d17b68558a1efcc1e97b85a58b2331838
Philippe Carvalho <philippe.santos@portergroup.com.br>
2021-12-21 20:53:18 -0300

    adds swagger

--- src/server.js
+++ src/server.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const obrasRoutes = require('./routes/obras');
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger.json')
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_URL);
 
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
 app.use(express.json());
 app.use('/obras', obrasRoutes);
-
-app.get('/', (req, res) => {
-	res.redirect('https://github.com/philippecarvalho/biblioteca-backend')
-})
 
 app.listen(process.env.PORT, () => {
   console.log(`online at ${process.env.PORT}`); 
