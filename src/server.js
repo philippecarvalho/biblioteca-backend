9b6c349c25317c7a58ccfe548e6270104182664d
Philippe Carvalho <philippe.santos@portergroup.com.br>
2021-12-21 20:06:48 -0300

    fixes port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 3000;
 const obrasRoutes = require('./routes/obras');
 
 const mongoose = require('mongoose');
@@ -10,6 +9,10 @@ mongoose.connect(process.env.DB_URL);
 app.use(express.json());
 app.use('/obras', obrasRoutes);
 
-app.listen(PORT, () => {
-  console.log(`online at ${PORT}`);
+app.get('/', (req, res) => {
+	res.redirect('https://github.com/philippecarvalho/biblioteca-backend')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`online at ${process.env.PORT}`); 
 });
